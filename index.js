be733bdf9a6ca56b57d9b624776113c34cf56d94
EthanHaley <ethan.haley93@gmail.com>
2017-12-14 03:40:36 -0800

    Everything function for baseline.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var fs = require('fs');
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var Promise = require('promise');
+var url = "mongodb://localhost:27017/rundnd";
 
-var port = 8080;
+var uri = "mongodb://ehaley:Chr0n0k33p3r@ethanhaley-shard-00-00-n5zg9.mongodb.net:27017,ethanhaley-shard-00-01-n5zg9.mongodb.net:27017,ethanhaley-shard-00-02-n5zg9.mongodb.net:27017/test?ssl=true&replicaSet=EthanHaley-shard-0&authSource=admin";
+
+var port = 1357;
 var stylesheet = fs.readFileSync('public/app.css');
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'html');
-app.engine('html', require('hbs')._express);
+app.engine('html', require('hbs').__express);
 
 app.get('/', function(req, res) {
 	fs.readFile('public/index.html', function(err, body) {
@@ -24,6 +33,193 @@ app.get('/app.js', function(req, res) {
 app.get('/app.css', function(req, res) {
 	res.setHeader('Content-Type', 'text/css');
 	res.end(stylesheet);
+});
+
+app.get('/navbar.html', function(req, res) {
+	res.render('navbar');
+});
+
+app.get('/home.html', function(req, res) {
+	res.render('home');
+});
+
+app.get('/monsters.html', function(req, res) {
+	MongoClient.connect(uri, function(err, db) {
+		if(err) throw err;
+		db.collection("characters").find({'class': 'Monster', 'race': 'Monster'}).toArray(function(err, result) {
+			if(err) throw err;
+			var context = {};
+			context.characters = result;
+			res.render('characters', context)
+			db.close();
+		});
+	});
+});
+
+app.get('/character.html', function(req, res) {
+	res.render('character');
+});
+
+app.post('/editCharacter.html', function(req, res) {
+	var request = req.body;
+	if(!request.charId) { res.render('character') }
+	else {
+		MongoClient.connect(uri, function(err, db) {
+			if(err) throw err;
+			db.collection("characters").findOne({'_id': ObjectId(request.charId.toString())}, function(err, result) {
+				if(err) throw err;
+				res.render('character', result);
+			});
+		});
+	}
+});
+
+app.post('/deleteCharacter.html', function(req, res) {
+	var request = req.body;
+	MongoClient.connect(uri, function(err, db) {
+		if(err) throw err;
+		db.collection("characters").deleteOne({'_id': ObjectId(request.charId.toString())}, function(err, result) {
+			if(err) throw err;
+			db.collection("characters").find({}).toArray(function(err, result) {
+				if(err) throw err;
+				console.log("1 character deleted");
+				var context = {};
+				context.characters = result;
+				res.render('characters', context)
+				db.close();
+			});
+		});
+	});
+})
+
+
+app.post('/startBattle', function(req, res) {
+	var request = req.body;
+	context = {};
+	context.characters = [];
+	for(key in request) {
+		context.characters.push(request[key]);
+	}
+	res.render('battle', context);
+	/*MongoClient.connect(uri, function(err, db) {
+		if(err) throw err;
+		request.keys()
+		db.collection("characters").find({'_id': {'in': }}).toArray(function(err, result) {
+			if(err) throw err;
+			var context = {};
+			context.characters = result;
+			res.render('battle', context)
+			db.close();
+		});
+	});*/
+});
+
+app.post('/battle-template.html', function(req, res) {
+	var request = req.body;
+	MongoClient.connect(uri, function(err, db) {
+		if(err) throw err;
+		db.collection("characters").findOne({'_id': ObjectId(request.id.toString())}, function(err, result) {
+			if(err) throw err;
+			res.render('battle-template', result);
+		});
+	});
+});
+
+app.get('/characters.html', function(req, res) {
+	MongoClient.connect(uri, function(err, db) {
+		if(err) throw err;
+		db.collection("characters").find({}).toArray(function(err, result) {
+			if(err) throw err;
+			var context = {};
+			context.characters = result;
+			res.render('characters', context)
+			db.close();
+		});
+	});
+});
+
+app.get('/encounter.html', function(req, res) {
+	MongoClient.connect(uri, function(err, db) {
+		if(err) throw err;
+		db.collection("characters").find({}).toArray(function(err, result) {
+			if(err) throw err;
+			var context = {};
+			context.characters = result;
+			res.render('encounter', context)
+			db.close();
+		});
+	});
+});
+
+app.post('/saveCharacter', function(req, res) {
+	var request = req.body;
+	if (request.id) {
+		MongoClient.connect(uri, function(err, db) {
+			if(err) throw err;
+			db.collection("characters").updateOne({'_id': ObjectId(request.id.toString())}, 
+				{
+					$set: {
+						'name': request.name,
+						'hp': request.hp,
+						'ac': request.ac,
+						'speed': request.speed,
+						'str': request.str,
+						'dex': request.dex,
+						'con': request.con,
+						'int': request.int,
+						'wis': request.wis,
+						'char': request.char,
+						'class': request.class,
+						'race': request.race,
+						'description': request.description,
+						'proficiency': request.proficiency
+					}
+				}, function(err, result) {
+					if(err) throw err;
+					console.log("1 character edited");
+					db.collection("characters").find({}).toArray(function(err, result) {
+						if(err) throw err;
+						var context = {};
+						context.characters = result;
+						res.render('characters', context)
+						db.close();
+					});
+				});
+		});
+	}
+	else {
+		MongoClient.connect(uri, function(err, db) {
+			if(err) throw err;
+			var character = {
+				name: request.name,
+				hp: request.hp, 
+				ac: request.ac,
+				speed: request.speed,
+				str: request.str,
+				dex: request.dex,
+				con: request.con,
+				int: request.int,
+				wis: request.wis,
+				char: request.char,
+				class: request.class,
+				race: request.race,
+				description: request.description,
+				proficiency: request.proficiency,
+				//image: request.image
+			};
+			db.collection("characters").insertOne(character, function(err, result) {
+				if(err) throw err;
+				console.log("1 character saved");
+				db.collection("characters").find({}).toArray(function(err, result) {
+					if(err) throw err;
+					var context = {};
+					context.characters = result;
+					res.render('characters', context)
+					db.close();
+				});
+			});
+		});
+	}
 });
 
 
--- public/app.js
+++ public/app.js
@@ -1,5 +1,133 @@
 function loadPage() {
-	$('body').load('/view.html');
+	$('body').load('/home.html');
+}
+
+function characters() {
+	$('body').load('/characters.html');
+}
+
+function monsters() {
+	$('body').load('/monsters.html');
+}
+
+function editCharacter(index) {
+	var editSelect = "#id-" + index;
+	var charId = $(editSelect).val();
+	$('body').load('/editCharacter.html', {'charId': charId});
+}
+
+function deleteCharacter(index) {
+	var deleteSelect = "#id-" + index;
+	var charId = $(deleteSelect).val();
+	$('body').load('/deleteCharacter.html', {'charId': charId});
+}
+
+function saveCharacter() {
+	character = {};
+	character.id = $('#charId').val();
+	character.name = $('#name').val();
+	character.hp = $('#hp').val();
+	character.ac = $('#ac').val();
+	character.speed = $('#speed').val();
+	character.str = $('#str').val();
+	character.dex = $('#dex').val();
+	character.con = $('#con').val();
+	character.int = $('#int').val();
+	character.wis = $('#wis').val();
+	character.char = $('#char').val();
+	character.class = $('#class').val();
+	character.race = $('#race').val();
+	character.proficiency = $('#proficiency').val();
+	character.description = $('#description').val();
+	//character.image = $('#image').val();
+	$('body').load('/saveCharacter', character);
+}
+
+function addToEncounter() {
+	var charCount = $('#char-count').val();
+	var selectedId = $('#addCharacter').val();
+	var selectedCharacter = $('#addCharacter option:selected').attr('id');
+	var trString = '<tr id="row-' + charCount + '"><td>' + selectedCharacter + '</td><td><input type="number" id="' + selectedId + '" required></td><td><button class="btn btn-danger" onclick="removeFromInit(' + charCount + ')">X</button></td></tr>';
+	$('#char-count').val(Number($('#char-count').val()) + 1);
+	if (($('#char-count').val()) > 1) {
+		$('#start-button').attr('disabled', false);
+	}
+	$('#character-table tr:last').after(trString);
+}
+
+function removeFromInit(charCount) {
+	var selectString = '#row-' + charCount;
+	$(selectString).remove();
+	$('#char-count').val(Number($('#char-count').val()) - 1);
+	if (($('#char-count').val()) < 2) {
+		$('#start-button').attr('disabled', true);
+	}
+}
+
+function goToEncounter() {
+	$('body').load('/encounter.html');
+}
+
+function goToBattle() {
+	$('body').load('/battle.html');
+}
+
+function subHp(index) {
+	var subSelect = "#hpAddSub-" + index;
+	var sub = $(subSelect).val();
+	var curSelect = "#hp-" + index;
+	var curHp =  $(curSelect).val();
+	var newHp = Number(curHp) - Number(sub);
+	if(newHp < 0 ) {
+		newHp = 0;
+	}
+	var replaceText = "Hit Points: " + newHp;
+	var replaceBtn = '#hpBtn-' + index; 
+	$(curSelect).val(newHp);
+	$(replaceBtn).html(replaceText);
+}
+
+function addHp(index) {
+	var addSelect = "#hpAddSub-" + index;
+	var add = $(addSelect).val();
+	var curSelect = "#hp-" + index;
+	var curHp =  $(curSelect).val();
+	var newHp = Number(curHp) + Number(add);
+	var maxSelect = "#max-hp-" + index;
+	var maxHp = $(maxSelect).val();
+	if(newHp > Number(maxHp) ) {
+		newHp = Number(maxHp);
+	}
+	var replaceText = "Hit Points: " + newHp;
+	var replaceBtn = '#hpBtn-' + index; 
+	$(curSelect).val(newHp);
+	$(replaceBtn).html(replaceText);
+}
+
+function startBattle() {
+	var tempDict = {};
+	var inits = [];
+	var index = 0;
+	$('#character-table tr input').each(function() {
+		inits[index] = this.value;
+		tempDict[index] = {'init': this.value, 'id': this.id};
+		index++;
+	});
+	inits.sort(function(a, b) {
+		return b-a
+	});
+	var initOrder = [];
+	inits.forEach(function(init) {
+		for(arrayNum in tempDict) {
+			if(tempDict[arrayNum].init == init) {
+				initOrder.push({'id': tempDict[arrayNum].id});
+			}
+		}
+	});
+	console.log(initOrder);
+	var context = {};
+	context.initOrder = initOrder;
+	$('body').load('/startBattle', context);
 }
 
 loadPage();
